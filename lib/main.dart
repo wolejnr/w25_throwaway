46d17bd20bb4dc1dd72f55f427d467a3b31cc5e1
Adewole Adewumi <wolejr@gmail.com>
2025-02-02 06:51:40 -0500

    Done with week 3

--- lib/main.dart
+++ lib/main.dart
@@ -13,25 +13,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'ABCDE'),
     );
   }
 }
@@ -61,88 +47,88 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    
     return Scaffold(
+      drawer: const Drawer(child: Text("Hi There"),),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        
         title: Text(widget.title),
+        actions: [
+          OutlinedButton(onPressed: (){}, child: const Text("Button 1")),
+          const SizedBox(width: 10.0),
+          OutlinedButton(onPressed: (){}, child: const Text("Button 2"))
+        ],
+        
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Image.asset('images/ac_logo.jpg', width: 200),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Slider(
-              value: _counter,
-              min: 0,
-              max: 100,
-              onChanged: (value) {
-                setState(() {
-                  _counter = value;
-                });
-              },
-            ),
-            Switch(value: isChecked, onChanged: (value){
-              setState(() {
-                isChecked = value;
-              });
-            }),
-            Checkbox(value: isChecked, onChanged: (value){
-              setState(() {
-                isChecked = value!;
-              });
-            })
-          ],
-        ),
+        child: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  const Icon(Icons.call),
+                  Text(
+                    "Call".toUpperCase(),
+                    style: const TextStyle(color: Colors.red),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  const Icon(
+                    Icons.send,
+                    color: Colors.teal,
+                  ),
+                  Text(
+                    "Route".toUpperCase(),
+                    style: const TextStyle(color: Colors.red),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  const Icon(
+                    Icons.share,
+                    color: Colors.teal,
+                  ),
+                  Text("Share".toUpperCase(),
+                      style: const TextStyle(color: Colors.red))
+                ],
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Stack(
+            alignment: AlignmentDirectional.bottomEnd,
+            children: [
+              Image.asset("images/ac_logo.jpg", width: 400),
+              const Text(
+                "Algonquin College",
+                style: TextStyle(fontSize: 30.0, backgroundColor: Colors.white),
+              )
+            ],
+          )
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar:  BottomNavigationBar(items: const  [
+        BottomNavigationBarItem(icon: Icon(Icons.camera), label: "Camera"),
+        BottomNavigationBarItem(icon: Icon(Icons.add_call), label: 'Phone'),
+      ], onTap: (btnIndex){
+        if(btnIndex == 0) print("Camera button pressed");
+        else print("Phone button pressed");
+      },), 
     );
   }
 }
